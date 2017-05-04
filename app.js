1f09ac1614a0ee46db4e08acbc6bcf7ccc478ade
Giulio Montenero <giuliomontenero@giulios-mbp.mul.ie.ibm.com>
2017-05-04 13:27:05 +0100

    Adding watson conversation service

--- app.js
+++ app.js
@@ -21,6 +21,55 @@ app.use(express.static(__dirname + '/public'));
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
+
+
+// watson conversation test start
+
+var prompt = require('prompt-sync')();
+// Example 1: sets up service wrapper, sends initial message, and
+// receives response.
+var ConversationV1 = require('watson-developer-cloud/conversation/v1');
+
+// Set up Conversation service wrapper.
+var conversation = new ConversationV1({
+  username: 'e304d642-6a57-467b-b8ee-d77ab6e0a87a', // replace with username from service key
+  password: 'JaUAup0iwIo2', // replace with password from service key
+  path: { workspace_id: 'dc5d1bc3-527a-46ef-8530-a307f419329a' }, // replace with workspace ID
+  version_date: '2016-07-11'
+});
+
+// Start conversation with empty message.
+conversation.message({}, processResponse);
+
+// Process the conversation response.
+function processResponse(err, response) {
+  if (err) {
+    console.error(err); // something went wrong
+    return;
+  }
+
+  // If an intent was detected, log it out to the console.
+  if (response.intents.length > 0) {
+    console.log('Detected intent: #' + response.intents[0].intent);
+  }
+
+  // Display the output from dialog, if any.
+  if (response.output.text.length != 0) {
+      console.log(response.output.text[0]);
+  }
+
+  // Prompt for the next round of input.
+  var newMessageFromUser = prompt('>> ');
+  conversation.message({
+    input: { text: newMessageFromUser }
+    }, processResponse)
+}
+
+
+// watson conversation test end
+
+
+
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
